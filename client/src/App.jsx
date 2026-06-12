3185916540edac7d1fe829769a042c672b4f6048
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-06-12 22:56:05 +0100

    Styled login page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
@@ -15,30 +16,43 @@ function App() {
   }
 
   return (
-    <>
-      <section id="center">
-        <div className="login-container" >
-          <input id="username" placeholder="Username / Email" type="text"/>
-          <input id="password" placeholder="Username / Email" type="text"/>
-          <button
-            type="button"
-            className="counter"
-            onClick={() => console.log("Hello")}
-          >Login
-          </button>
-          <button
-            type="button"
-            className="counter"
-            onClick={() => console.log("Hello")}
-          >Forgot Password
-          </button>
+    <div id="ui-container">
+      <section id="display-information">
+        <div id="profile-picture"></div>
+        <h1>Employee Portal</h1>
+        <h2>Imaginary Company platform</h2>
+      </section>
+      <section id="login-fields">
+        <div id="login-container" >
+          <div id="gap1" className="gap" style={{height:"5vh"}}></div>
+            <div id="heading">
+              <h1>Sign in</h1>
+              <h2>Enter your credentials to access employee portal</h2>
+            </div>
+            <div id="gap2" className="gap"style={{height:"5vh"}}></div>
+            <div id="inputs">
+              <p>USERNAME</p>
+              <input id="username" className="text-input" placeholder="Username" type="text"/>
+              <p>PASSWORD</p>
+              <input id="password" className="text-input" placeholder="Password" type="text"/>
+            </div>
+            <div id="buttons">
+              <button
+                type="button"
+                className="button"
+                onClick={() => handleLogin()}
+              >Login
+              </button>
+              <button
+                type="button"
+                className="button"
+                onClick={() => handleLogin()}
+              >Forgot Password
+              </button>
+            </div>
         </div>
       </section>
-      <section>
-        
-      </section>
-
-    </>
+    </div>
   )
 }
 
