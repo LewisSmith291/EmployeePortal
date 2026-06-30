07d17f4b7acce6e3324a8da242af384ae14ce289
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2026-06-30 21:22:55 +0100

    Added navigation functions to home page header

--- client/src/pages/PortalPage.jsx
+++ client/src/pages/PortalPage.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom'
-
+import '../portal.css'
 function PortalPage({ currentUser, onLogout }) {
   const navigate = useNavigate();
 
@@ -8,8 +8,37 @@ function PortalPage({ currentUser, onLogout }) {
     navigate("/login", { replace: true });
   }
 
+  function handleHomePress(){
+    // replace: true - adds onto stack (/portal/home/specific-thing)
+    // replace: false - replaces route (Will be "/portal")
+    navigate("/portal", { replace: true });
+  }
+
+  function handleProfilePress(){
+    navigate("/employee-profile", { replace:false });
+  }
+
+  function handleTimeOffPress(){
+    navigate("/time-off", { replace:false });
+  }
+
+  function handlePayslipsPress(){
+    navigate("/payslips", { replace:false });
+  }
+
   return (
     <div>
+      <header id="portal-header">
+        <div id="portal-profile-picture" onClick={handleHomePress}></div>
+        <nav>
+          <ul>
+            <li onClick={handleHomePress}>Home</li>
+            <li onClick={handleProfilePress}>Employee Profile</li>
+            <li onClick={handleTimeOffPress}>Time Off</li>
+            <li onClick={handlePayslipsPress}>Payslips</li>
+          </ul>
+        </nav>
+      </header>
       <h1>Welcome, {currentUser.firstName} {currentUser.lastName}</h1>
       {currentUser.role === "admin" && (
         <p><Link to="/employees">View Employees</Link></p>
@@ -19,4 +48,4 @@ function PortalPage({ currentUser, onLogout }) {
   );
 }
 
-export default PortalPage
+export default PortalPage;
